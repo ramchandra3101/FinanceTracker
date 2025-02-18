08559158c6739e7af0d913bfb2095c0107fe1133
ramchandra3101 <ramchandra3101@gmail.com>
2025-02-18 06:57:27 -0500

    checking commit profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import AuthContainer from "@/components/AuthContainer";
+import Background from "@/public/Background/Background.jpg";
 
 export default function Home() {
   return(
-    <div>
-      <AuthContainer/>
-
+    <div className="relative h-screen">
+      <Image
+        src={Background}
+        alt="Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+      />
+      <div className="absolute inset-0 bg-black opacity-70"></div>
+      
+        <AuthContainer />
     </div>
-
+  
   )
 }
---
+++ app/pages/Tracker/page.tsx
@@ -0,0 +1,11 @@
+import exp from "constants";
+
+const Tracker: React.FC = () => {
+  return (
+    <div>
+        <h1> Welcome page</h1>
+      <h1>Tracker</h1>
+    </div>
+  );
+}
+export default Tracker;
